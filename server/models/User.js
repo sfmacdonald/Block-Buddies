0addbef313a1e6e16c6010ce22ca76886041412f
floconno <floconno@asu.edu>
2024-04-04 21:33:30 -0700

    trying to fix resolvers

--- server/models/User.js
+++ server/models/User.js
@@ -19,10 +19,10 @@ const userSchema = new Schema({
     required: true,
     minlength: 8,
   },
-  sets: [
+  builds: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Set',
+      ref: 'Build',
     },
   ],
 });
