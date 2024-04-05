ddbaead36b6c3de6abe7bf396399bb9eb42dd8ba
sourylime <145071829+sourylime@users.noreply.github.com>
2024-04-04 21:13:23 -0700

    message

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchForm from '../components/SearchForm';
 import SearchResults from '../components/SearchResults';
 import LEGOImage from '../assets/LEGOppl.jpg';
+import SingleRandomBuild from '../components/singleRandomBuild';
 
 const Home = () => {
   return (
@@ -15,9 +16,9 @@ const Home = () => {
       <SearchResults />
 
       <p>Block Buddies is designed for LEGO set constructors to track their completed builds and wishlist future projects. Builders can manage their profile, create wishlists, track current builds, and interact with other users.</p>
-
+      <SingleRandomBuild />
       <div className='image-container'>
-      <img src={LEGOImage} alt='LEGO People' />
+        <img src={LEGOImage} alt='LEGO People' />
       </div>
     </div>
   );
